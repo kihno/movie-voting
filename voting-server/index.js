6b7d32c48d597a3b79fee02b235ca638dd1994a4
kihno <stistyle13@gmail.com>
2024-03-28 16:56:19 -0700

    Add websocket to server

--- voting-server/index.js
+++ voting-server/index.js
@@ -1,3 +1,11 @@
-import makeStore from "./src/store"
+import makeStore from "./src/store.js"
+import startServer from "./src/server.js"
 
 export const store = makeStore()
+startServer(store)
+
+store.dispatch({
+    type: 'SET_ENTRIES',
+    entries: require('./entries.json')
+})
+store.dispatch({ type: 'NEXT' })
